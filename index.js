5287adcb37f4c32cf0f6f72c23cd8037f041e77f
obasemola <obasemolaayodele@gmail.com>
2020-06-14 02:06:40 +0300

    created dom element for some movie content

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com', {
         params: {
             apikey: '6ac39780',
-            i: searchTerm
+            s: searchTerm
         }
     });
-console.log(response.data)
+    return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const onInput = (e) => {
-        fetchData(e.target.value);
+const onInput = async (e) => {
+    const movies = await fetchData(e.target.value);
+    movies.forEach((movie) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+        `;
+
+        document.querySelector('#content').appendChild(div)
+    });
 };
 
 input.addEventListener('input', debounce(onInput, 500));
