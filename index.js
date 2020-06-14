feefc9c0afb5341a493a964e7b7d70b692231605
obasemola <obasemolaayodele@gmail.com>
2020-06-14 23:35:34 +0300

    refactoring the code to make the autocomplete logic reusable

--- index.js
+++ index.js
@@ -13,57 +13,9 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
-const root = document.querySelector(".autocomplete");
-root.innerHTML = `
-    <label><b>Search For a Movie</b></label>
-    <input class="input" />
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
-    </div>
-`;
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (e) => {
-    const movies = await fetchData(e.target.value);
-
-    if(!movies.length) {
-        dropdown.classList.remove('is-active');
-        return;
-    }
-
-    resultsWrapper.innerHTML = '';
-    dropdown.classList.add('is-active');
-    movies.forEach((movie) => {
-        const option = document.createElement('a');
-        const imgSrc = movie.Poster === "N/A" ? '' : movie.Poster;
-
-        option.classList.add('dropdown-item');
-        option.innerHTML = `
-        <img src="${imgSrc}" />
-        ${movie.Title}
-        `;
-
-        option.addEventListener('click', (e) => {
-            dropdown.classList.remove('is-active');
-            input.value = movie.Title;
-            onMovieSelect(movie);
-        });
-
-        resultsWrapper.appendChild(option)
-    });
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', (e) => {
-    if (!root.contains(e.target)) {
-        dropdown.classList.remove('is-active')
-    }
-});
+createAutoComplete({ root: document.querySelector('.autocomplete') });
+createAutoComplete({ root: document.querySelector('.autocomplete-two') });
+createAutoComplete({ root: document.querySelector('.autocomplete-three') });
 
 const onMovieSelect = async (movie) => {
     const response = await axios.get('http://www.omdbapi.com', {
