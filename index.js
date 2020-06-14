3b68029464d9ca8669a4f6180e06ef2a5e68adfa
obasemola <obasemolaayodele@gmail.com>
2020-06-14 11:25:06 +0300

    moved element code from html to js file

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
         }
     });
+
+    if (response.data.Error) {
+        return []
+    };
+
     return response.data.Search;
 };
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async (e) => {
     const movies = await fetchData(e.target.value);
